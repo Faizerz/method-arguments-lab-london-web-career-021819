5297343e5e44ae3a73175e4ffd99c1f222a309cf
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-09 17:41:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,8 @@ end
 
 introduction("Josh")
 
-introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in Ember.js.\n"
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+introduction_with_language("Dan", "Ember.Josh")
